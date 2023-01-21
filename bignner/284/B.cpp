d6b5c942888e7a27508f5e9d8e18b767b5e54c42
Andyyyy64 <takuyarossi77@gmail.com>
2023-01-21 22:53:31 +0900

    286

---
+++ bignner/284/B.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  int T;
+  for (int i = 0; i < T; i++) {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    
+  }
+}
---
+++ bignner/bootcamp/Easy/139Bans.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ int A,B;
+ cin >> A >> B;
+ int ans = (B - 1 + A - 2) / (A - 1);
+ cout << ans << endl;
+ return 0;
+}
